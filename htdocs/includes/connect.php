f44b3bdc2db7b200f012a21edae70757db270f5d
Dendakoduong <106602391+Dendakoduong@users.noreply.github.com>
2023-01-03 20:26:57 +0700

    Update connect.php

--- htdocs/includes/connect.php
+++ htdocs/includes/connect.php
@@ -1,5 +1,6 @@
 <?php
-$con = mysqli_connect('localhost', 'root', '', 'asm2_web');
+$con = mysqli_connect('sql203.epizy.com', 'epiz_33271161', 'XhoCwt3Y9AcvHB', 'epiz_33271161_asm2_web');
 if (!$con) {
   echo "disconnected to database";
 }
+?>
